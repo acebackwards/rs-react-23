5a234ef9f9c4d0a91965a5c031712848c48937ae
Backwards <deema.light@gmail.com>
2023-03-18 22:37:26 +0300

    feat: add header stylization

--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import { Input, NavLink } from '../../shared/ui';
+import { NavLink } from '../../shared/ui';
 import s from './style.module.css';
 
 export class Header extends Component {
   render() {
     return (
       <nav className={s.nav}>
-        <NavLink title="Home" link="/" />
-        <NavLink title="About Us" link="/about" />
-        <Input placeholder="Поиск..." />
+        <div className={s.current}>Current Page</div>
+        <div className={s.project}>Project Name</div>
+        <div>
+          <NavLink title="Home" link="/" />
+          <NavLink title="About Us" link="/about" />
+        </div>
       </nav>
     );
   }

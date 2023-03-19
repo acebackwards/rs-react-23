2de079d324e6a7f0e7adc3bd2df6e5d3d0f75892
Backwards <deema.light@gmail.com>
2023-03-19 22:53:27 +0300

    feat: add tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    coverage: {
+      exclude: ['src/main.tsx'],
+      all: true,
+      src: ['src'],
+      provider: 'c8',
+      reporter: ['text'],
+    },
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+  },
 });

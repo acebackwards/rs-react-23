eb433024f4abcdd75de96d80f0eb913c418fa9ef
Backwards <deema.light@gmail.com>
2023-03-19 22:55:02 +0300

    refactor: move header to app component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MAIN_PAGE_ROUTE, ABOUT_PAGE_ROUTE } from './consts/routes';
 import { MainPage, AboutPage, ErrorPage } from '../pages';
+import { Header } from '../widgets';
+
 import './index.css';
 
 export class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path={MAIN_PAGE_ROUTE} element={<MainPage />} />
           <Route path={ABOUT_PAGE_ROUTE} element={<AboutPage />} />

89087161826e09b6448c8e8cc0a3ca370ebd5d95
Backwards <deema.light@gmail.com>
2023-03-19 22:57:14 +0300

    feat: add props to card and stylization

--- src/entities/ui/Card/index.tsx
+++ src/entities/ui/Card/index.tsx
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import s from './style.module.css';
 
-export class Card extends Component {
+type CardProps = {
+  name: string;
+  status: string;
+  species: string;
+  img: string;
+  location: string;
+};
+
+export class Card extends Component<CardProps> {
   render() {
     return (
       <div className={s.card}>
         <div className={s['image-container']}>
-          <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" />
+          <img src={this.props.img} alt="" />
         </div>
         <div className={s['description-container']}>
           <div>
-            <h3>Rick Sanchez</h3>
+            <h3>{this.props.name}</h3>
           </div>
           <div className={s.description}>
-            <div className={s.type}>Human</div>
-            <div className={s.status}>Alive</div>
+            <div className={s.type}>{this.props.species}</div>
+            <div className={s.status}>{this.props.status}</div>
           </div>
           <div className={s.location}>
-            <span>Location:</span> Earth
+            <span>Location:</span> {this.props.location}
           </div>
         </div>
       </div>

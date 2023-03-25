2142936c14e28304b641e9b6c14852ae446a0589
Backwards <deema.light@gmail.com>
2023-03-26 00:58:40 +0300

    feat: update button types

--- src/shared/ui/Button/index.tsx
+++ src/shared/ui/Button/index.tsx
@@ -3,10 +3,18 @@ import s from './style.module.css';
 
 type ButtonProps = {
   title: string;
+  type?: ButtonType;
+  className?: string;
 };
+
+type ButtonType = 'button' | 'submit';
 
 export class Button extends Component<ButtonProps> {
   render() {
-    return <button className={s.button}>{this.props.title}</button>;
+    return (
+      <button type={this.props.type || 'button'} className={s.button + ' ' + this.props.className}>
+        {this.props.title}
+      </button>
+    );
   }
 }

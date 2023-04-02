b749e9d9f4df7bbd661a768089ffe9798ab3c942
Backwards <deema.light@gmail.com>
2023-04-03 02:08:01 +0300

    refactor: rewrite form page with react hook form

--- src/entities/ui/FormCard/index.tsx
+++ src/entities/ui/FormCard/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import s from './style.module.css';
 
 type FormCardProps = {
@@ -7,12 +7,12 @@ type FormCardProps = {
   date: string;
   img: string;
   author: boolean;
-  gender: boolean;
+  gender: string;
 };
 
-export class FormCard extends Component<FormCardProps> {
-  recognizeType() {
-    switch (this.props.type) {
+export const FormCard = (props: FormCardProps) => {
+  const recognizeType = () => {
+    switch (props.type) {
       case '1':
         return 'Funny';
       case '2':
@@ -22,28 +22,26 @@ export class FormCard extends Component<FormCardProps> {
       case '4':
         return 'IT';
     }
-  }
+  };
 
-  render() {
-    return (
-      <div className={s.card}>
-        <div className={s['image-container']}>
-          <img src={this.props.img} alt="" />
-        </div>
-        <div className={s['description-container']}>
-          <div>
-            <h3>{this.props.title}</h3>
-          </div>
-          <div className={s.description}>
-            <div className={s.type}>{this.recognizeType()}</div>
-            <div className={s.date}>{this.props.date}</div>
-          </div>
-          <div className={s.author}>
-            <span>Usage Rights:</span> {this.props.author ? 'Yes' : 'No'}
-          </div>
-          <div className={s.author}>Created by {this.props.gender ? 'Male' : 'Female'}</div>
-        </div>
+  return (
+    <div className={s.card}>
+      <div className={s['image-container']}>
+        <img src={props.img} alt="" />
       </div>
-    );
-  }
-}
+      <div className={s['description-container']}>
+        <div>
+          <h3>{props.title}</h3>
+        </div>
+        <div className={s.description}>
+          <div className={s.type}>{recognizeType()}</div>
+          <div className={s.date}>{props.date}</div>
+        </div>
+        <div className={s.author}>
+          <span>Is developer:</span> {props.author ? 'Yes' : 'No'}
+        </div>
+        <div className={s.author}>Created by {props.gender}</div>
+      </div>
+    </div>
+  );
+};
--- src/pages/FormPage/index.tsx
+++ src/pages/FormPage/index.tsx
@@ -1,188 +1,153 @@
-import React, { Component, createRef, RefObject } from 'react';
+import React, { useState } from 'react';
 import s from './style.module.css';
 import { Button } from '../../shared/ui';
 import { FormCard } from '../../entities/ui';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 type ItemType = {
   title: string;
   date: string;
   type: string;
-  author: boolean;
-  gender: boolean;
+  developer: boolean;
+  gender: string;
   file: string;
 };
 
-export class FormPage extends Component<Record<string, never>> {
-  formList: ItemType[] = [];
-  titleField: RefObject<HTMLInputElement> = createRef();
-  dataField: RefObject<HTMLInputElement> = createRef();
-  typeField: RefObject<HTMLSelectElement> = createRef();
-  authorField: RefObject<HTMLInputElement> = createRef();
-  genderMaleField: RefObject<HTMLInputElement> = createRef();
-  genderFemaleField: RefObject<HTMLInputElement> = createRef();
-  fileField: RefObject<HTMLInputElement> = createRef();
+interface IForm {
+  title: string;
+  date: string;
+  type: string;
+  developer: boolean;
+  gender: string;
+  file: FileList;
+}
 
-  state: {
-    formList: ItemType[];
-    titleIsValid: boolean;
-    dateIsValid: boolean;
-    genderIsValid: boolean;
-    fileIsValid: boolean;
-  } = {
-    formList: [],
-    titleIsValid: true,
-    dateIsValid: true,
-    genderIsValid: true,
-    fileIsValid: true,
-  };
+interface IFormCard {
+  title: string;
+  date: string;
+  type: string;
+  developer: boolean;
+  gender: string;
+  file: string;
+}
 
-  validateTitle() {
-    return this.titleField.current?.value === '';
-  }
+export const FormPage = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<IForm>();
 
-  validateDate() {
-    return this.dataField.current?.value === '';
-  }
+  const [formList, setFormList] = useState<ItemType[]>([]);
 
-  validateGender() {
-    return !this.genderMaleField.current?.checked && !this.genderFemaleField.current?.checked;
-  }
-
-  validateFile() {
-    return (
-      ((this.fileField.current as HTMLInputElement).files as FileList)[0] &&
-      ((this.fileField.current as HTMLInputElement).files as FileList)[0].type.slice(0, 5) ===
-        'image'
-    );
-  }
-
-  sendData = (e: React.FormEvent | HTMLFormElement) => {
-    e.preventDefault();
-    const title = (this.titleField.current as HTMLInputElement).value;
-    const date = (this.dataField.current as HTMLInputElement).value;
-    const type = (this.typeField.current as unknown as HTMLSelectElement).value;
-    const author = (this.authorField.current as HTMLInputElement).checked;
-    const genderMale = (this.genderMaleField.current as HTMLInputElement).checked;
-    const genderFemale = (this.genderFemaleField.current as HTMLInputElement).checked;
-    const file = ((this.fileField.current as HTMLInputElement).files as FileList)[0];
-
-    if (
-      title === '' ||
-      date === '' ||
-      (genderMale === false && genderFemale === false) ||
-      file.type.slice(0, 5) != 'image'
-    ) {
-      this.setState(() => {
-        return {
-          titleIsValid: !this.validateTitle(),
-          dateIsValid: !this.validateDate(),
-          genderIsValid: !this.validateGender(),
-          fileIsValid: this.validateFile(),
-        };
-      });
-      return;
-    }
-
-    this.setState(() => {
-      return {
-        titleIsValid: this.validateTitle(),
-        dateIsValid: this.validateDate(),
-        genderIsValid: this.validateGender(),
-        fileIsValid: !this.validateFile(),
-      };
-    });
-
-    this.formList.push({
+  const onSubmit: SubmitHandler<IForm> = ({
+    title,
+    date,
+    type,
+    developer,
+    gender,
+    file,
+  }: IForm) => {
+    const newFormCard: IFormCard = {
       title,
       date,
       type,
-      author,
-      gender: genderMale ? true : false,
-      file: window.URL.createObjectURL(file),
-    });
+      developer,
+      gender,
+      file: window.URL.createObjectURL(file[0]),
+    };
 
-    this.setState(() => {
-      return {
-        formList: [
-          ...this.formList,
-          { title, date, type, author, gender: genderMale ? true : false, file },
-        ],
-      };
-    });
-    alert('Card has been added successfully');
-    e.currentTarget.reset();
+    setFormList([...formList, newFormCard]);
+
+    reset();
   };
 
-  render() {
-    return (
-      <div className={s.formPage}>
-        <div className={s.container}>
-          <form
-            className={s.form}
-            onSubmit={(e) => {
-              this.sendData(e);
-            }}
-          >
-            <h3>Input your meme title</h3>
-            <input type="text" name="title" ref={this.titleField} />
-            {this.state.titleIsValid || <span>You have to fill this field...</span>}
+  return (
+    <div className={s.formPage}>
+      <div className={s.container}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="title">Meme title</label>
+          <input
+            placeholder="Type something..."
+            {...register('title', {
+              required: true,
+            })}
+          />
+          {errors.title && <span>You have to fill this field...</span>}
 
-            <label>Creation date</label>
-            <input type="date" name="date" ref={this.dataField} />
-            {this.state.dateIsValid || <span>You have to fill this field...</span>}
+          <label htmlFor="date">Creation date</label>
+          <input
+            type="date"
+            {...register('date', {
+              required: true,
+            })}
+          />
+          {errors.date && <span>You have to fill this field...</span>}
 
-            <label>Choose type</label>
-            <select name="type" ref={this.typeField}>
-              <option value="1">Funny</option>
-              <option value="2">Smart</option>
-              <option value="3">Science</option>
-              <option value="4">IT</option>
-            </select>
+          <label>Meme type</label>
+          <select {...register('type')}>
+            <option value="1">Funny</option>
+            <option value="2">Smart</option>
+            <option value="3">Science</option>
+            <option value="4">IT</option>
+          </select>
 
-            <div className={s.author}>
-              <label>Grant Usage Rights?</label>
-              <input type="checkbox" name="authorName" ref={this.authorField} />
-            </div>
-            <label>Choose your gender</label>
-            <p className={s.radioBlock}>
-              <div>
-                <input type="radio" name="userSex" value="sm" ref={this.genderMaleField} />
-                Male
-              </div>
-              <div>
-                <input type="radio" name="userSex" value="sf" ref={this.genderFemaleField} />
-                Female
-              </div>
-            </p>
-            {this.state.genderIsValid || <span>You have to choose...</span>}
+          <label>Is developer?</label>
+          <input type="checkbox" {...register('developer')} />
+
+          <label>Sex</label>
+          <div>
             <input
-              type="file"
-              name="meme"
-              accept="image/*"
-              className={s.file}
-              ref={this.fileField}
+              type="radio"
+              value="male"
+              {...register('gender', {
+                required: true,
+              })}
             />
-            {this.state.fileIsValid || <span>You have to upload image...</span>}
-            <Button type="submit" title="Submit" className={s.submit} />
-          </form>
-        </div>
+            Male
+          </div>
+          <div>
+            <input
+              type="radio"
+              value="female"
+              {...register('gender', {
+                required: true,
+              })}
+            />
+            Female
+          </div>
+          {errors.gender && <span>You have to choose...</span>}
 
-        <div className={s.cardContainer}>
-          {this.formList.map((item, index) => {
-            return (
-              <FormCard
-                key={index}
-                title={item.title}
-                date={item.date}
-                type={item.type}
-                gender={item.gender}
-                img={item.file}
-                author={item.author}
-              />
-            );
-          })}
-        </div>
+          <input
+            type="file"
+            accept="image/*"
+            className={s.file}
+            {...register('file', {
+              required: true,
+            })}
+          />
+          {errors.file && <span>You have to upload an image...</span>}
+
+          <Button type="submit" title="Submit" className={s.submit} />
+        </form>
       </div>
-    );
-  }
-}
+
+      <div className={s.cardContainer}>
+        {formList.map((item, index) => {
+          return (
+            <FormCard
+              key={index}
+              title={item.title}
+              date={item.date}
+              type={item.type}
+              gender={item.gender}
+              img={item.file}
+              author={item.developer}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};

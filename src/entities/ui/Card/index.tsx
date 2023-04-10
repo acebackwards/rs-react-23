c06d15adffa5b2b0067dd39d9bd10b794af7352a
Backwards <deema.light@gmail.com>
2023-04-10 23:30:48 +0300

    feat: add modal to card item

--- src/entities/ui/Card/index.tsx
+++ src/entities/ui/Card/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './style.module.css';
 
 type CardProps = {
@@ -10,23 +10,42 @@ type CardProps = {
 };
 
 export const Card = (props: CardProps) => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div className={s.card}>
-      <div className={s['image-container']}>
-        <img src={props.img} alt="" />
+    <>
+      {showModal && (
+        <div className={s.modal}>
+          <div className={s['modal__background']} onClick={() => setShowModal(false)} />
+          <div className={s['modal__card']}>
+            <div className={s['image-container']}>
+              <img src={props.img} alt="" />
+            </div>
+            <div className={s['description-container']}>
+              <div>
+                <h3>{props.name}</h3>
+              </div>
+              <div className={s.description}>
+                <div className={s.type}>{props.species}</div>
+                <div className={s.status}>{props.status}</div>
+              </div>
+              <div className={s.location}>
+                <span>Location:</span> {props.location}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className={s.card} onClick={() => setShowModal(true)}>
+        <div className={s['image-container']}>
+          <img src={props.img} alt="" />
+        </div>
+        <div className={s['description-container']}>
+          <div className={s['card__name']}>
+            <h3>{props.name}</h3>
+          </div>
+        </div>
       </div>
-      <div className={s['description-container']}>
-        <div>
-          <h3>{props.name}</h3>
-        </div>
-        <div className={s.description}>
-          <div className={s.type}>{props.species}</div>
-          <div className={s.status}>{props.status}</div>
-        </div>
-        <div className={s.location}>
-          <span>Location:</span> {props.location}
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
